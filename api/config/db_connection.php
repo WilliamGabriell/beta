62b95bf82faba0f7e650ab7d80217b10e26ff8f2
Gabriel <gabocadena610@gmail.com>
2024-09-20 12:18:35 -0500

    Endpoint: Database connection

---
+++ api/config/db_connection.php
@@ -0,0 +1,31 @@
+<?php
+/*$numero1 =20;
+$numero2 =30;
+$addition = $numero1 + $numero2;
+echo $addition;*/
+
+//Postgres database connection
+//Developer: Gabriel
+
+$host     = "localhost";//127.0.0.1
+$username = "postgres";
+$password = "unicesmag";
+$dbname   = "beta";
+$port     = "5432";
+
+$data_connection = " 
+host = $host
+port = $port
+user = $username
+password = $password";
+
+$conn = pg_connect( $data_connection);
+ 
+ if (!$conn) {
+    die("Connection failed: ". pg_last_error());
+ } else {
+    echo "Connected successfully";
+ }
+
+ pg_close($conn);
+?>
